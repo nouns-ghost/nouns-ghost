3b67ba8606530dd657d0b3ca36ea63540486fb55
Nouns Ghost <nouns.ghost@proton.me>
2022-08-20 22:22:46 +0900

    Remove reward to the operator

--- packages/nouns-contracts/contracts/NounsToken.sol
+++ packages/nouns-contracts/contracts/NounsToken.sol
@@ -147,13 +147,11 @@ contract NounsToken is INounsToken, Ownable, ERC721Checkpointable {
     }
 
     /**
-     * @notice Mint a Noun to the minter, along with a possible nounders reward
-     * Noun. Nounders reward Nouns are minted every 10 Nouns, starting at 0,
-     * until 183 nounder Nouns have been minted (5 years w/ 24 hour auctions).
+     * @notice Mint a Noun to the minter. The operator accepts reward of id == 0.
      * @dev Call _mintTo with the to address(es).
      */
     function mint(uint8 opacity) public override onlyMinter returns (uint256) {
-        if (_currentNounId <= 1820 && _currentNounId % 10 == 0) {
+        if (_currentNounId == 0) {
             _mintTo(noundersDAO, _currentNounId++, opacity);
         }
         return _mintTo(minter, _currentNounId++, opacity);
