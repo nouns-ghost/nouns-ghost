5c0d5189924ac5c70c1981e032d7bfd5ae33fd0e
Nouns Ghost <nouns.ghost@proton.me>
2022-09-02 13:22:57 +0900

    Update Hardhat configuration

--- packages/nouns-contracts/tasks/run-local.ts
+++ packages/nouns-contracts/tasks/run-local.ts
@@ -14,6 +14,7 @@ task(
   await run('populate-descriptor', {
     nftDescriptor: contracts.NFTDescriptor.instance.address,
     nounsDescriptor: contracts.NounsDescriptor.instance.address,
+    autoDeploy: true,
   });
 
   await contracts.NounsAuctionHouse.instance
